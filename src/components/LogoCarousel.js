03ff53daca192f452b15c6f6612ab88cb6ebd7fc
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-08-09 21:53:02 +0300

    NukaCarousel

--- src/components/LogoCarousel.js
+++ src/components/LogoCarousel.js
@@ -1,15 +1,56 @@
-import OwlCarousel from 'react-owl-carousel'
+/*import OwlCarousel from 'react-owl-carousel'*/
+import NukaCarousel from 'nuka-carousel'
+
 
 const LogoCarousel = ({options}) => {
 	return (
 		<div className="logo-carousel-section">
+
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-12">
+					<div className="">
+
+					<NukaCarousel
+						className=""
+						autoplay={true}
+						autoplayInterval={4500}
+						speed={450}
+						slidesToShow={4}
+						wrapAround={true}
+						cellAlign={'left'}
+						withoutControls={true}
+					>
+
+						<div className="single-logo-item">
+							<img src="images/company-logos/1.png" alt="" style={{display: "block", margin:"0 auto"}} />
+						</div>
+						<div className="single-logo-item">
+							<img src="images/company-logos/2.png" alt="" style={{display: "block", margin:"0 auto"}} />
+						</div>
+						<div className="single-logo-item">
+							<img src="images/company-logos/3.png" alt="" style={{display: "block", margin:"0 auto"}} />
+						</div>
+						<div className="single-logo-item">
+							<img src="images/company-logos/4.png" alt="" style={{display: "block", margin:"0 auto"}} />
+						</div>
+						<div className="single-logo-item">
+							<img src="images/company-logos/5.png" alt="" style={{display: "block", margin:"0 auto"}} />
+						</div>
+
+					</NukaCarousel>
+
+					</div>
+				</div>
+			</div>
+		</div>
 
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-12">
 						<div className="logo-carousel-inner">
 
-						<OwlCarousel className="owl-theme" {...options}>
+						{/*<Owl Carousel className="owl-theme" {...options}>
 
 							<div className="single-logo-item">
 								<img src="images/company-logos/1.png" alt="" />
@@ -27,7 +68,7 @@ const LogoCarousel = ({options}) => {
 								<img src="images/company-logos/5.png" alt="" />
 							</div>
 
-						</OwlCarousel>
+						</Owl Carousel>*/}
 
 						</div>
 					</div>
