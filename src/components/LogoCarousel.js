7ffb1288a984201674079d50632c1ea189daae81
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-08-09 22:13:15 +0300

    carousel items height

--- src/components/LogoCarousel.js
+++ src/components/LogoCarousel.js
@@ -12,30 +12,30 @@ const LogoCarousel = ({options}) => {
 					<div className="">
 
 					<NukaCarousel
-						className=""
 						autoplay={true}
 						autoplayInterval={4500}
 						speed={450}
 						slidesToShow={4}
 						wrapAround={true}
 						cellAlign={'left'}
+						adaptiveHeight={true}
 						withoutControls={true}
 					>
 
 						<div className="single-logo-item">
-							<img src="images/company-logos/1.png" alt="" style={{display: "block", margin:"0 auto"}} />
+							<img src="images/company-logos/1.png" alt="" style={{display: "block", margin:"0 auto", height:"100%"}} />
 						</div>
 						<div className="single-logo-item">
-							<img src="images/company-logos/2.png" alt="" style={{display: "block", margin:"0 auto"}} />
+							<img src="images/company-logos/2.png" alt="" style={{display: "block", margin:"0 auto", height:"100%"}} />
 						</div>
 						<div className="single-logo-item">
-							<img src="images/company-logos/3.png" alt="" style={{display: "block", margin:"0 auto"}} />
+							<img src="images/company-logos/3.png" alt="" style={{display: "block", margin:"0 auto", height:"100%"}} />
 						</div>
 						<div className="single-logo-item">
-							<img src="images/company-logos/4.png" alt="" style={{display: "block", margin:"0 auto"}} />
+							<img src="images/company-logos/4.png" alt="" style={{display: "block", margin:"0 auto", height:"100%"}} />
 						</div>
 						<div className="single-logo-item">
-							<img src="images/company-logos/5.png" alt="" style={{display: "block", margin:"0 auto"}} />
+							<img src="images/company-logos/5.png" alt="" style={{display: "block", margin:"0 auto", height:"100%"}} />
 						</div>
 
 					</NukaCarousel>
